74aa6d8d793566dd5639380edbfc1547867a753c
Megaterion <megaterion03@gmail.com>
2023-10-17 12:37:59 +0200

    dummie sprites added

--- lib/dnd_adventure.dart
+++ lib/dnd_adventure.dart
@@ -1,18 +1,24 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:dnd_adventure/levels/level.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 
 class DnDAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => Color.fromARGB(255, 237, 142, 142);
   late final CameraComponent cam;
 
   final world = Level();
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    // Lädt alle Bilder in den Cache
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
-        world: world, width: 640, height: 360);
+        world: world, width: 1280, height: 1280);
     cam.viewfinder.anchor = Anchor.topLeft;
     addAll([cam, world]);
     return super.onLoad();
